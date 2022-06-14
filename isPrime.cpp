df849d0cb27af25a13a65d98c757e5ed8e393b50
0xsagar <sagar.19bct@cmr.edu.in>
2022-06-15 00:30:50 +0530

    isPrime

---
+++ isPrime.cpp
@@ -0,0 +1,42 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 2; i < n; i++)
+//     {
+//         if (n % i == 0)
+//         {
+//             cout << "Not Prime" << endl;
+//             break;
+//         }
+//         else
+//         {
+//             cout << "Prime" << endl;
+//             break;
+//         }
+//     }
+// }
+
+// #include <iostream>
+// #include <cmath>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 2; i < sqrt(n); i++)
+//     {
+//         if (n % i == 0)
+//         {
+//             cout << "Not-Prime" << endl;
+//             break;
+//         }
+//         else
+//         {
+//             cout << "Prime" << endl;
+//             break;
+//         }
+//     }
+// }
