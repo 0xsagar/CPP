ad4ecab60f97acc9bea0d9593303c17bd86f519b
0xsagar <sagar.19bct@cmr.edu.in>
2022-06-15 00:20:23 +0530

    day 4: finished patterns with output

--- patterns.cpp
+++ patterns.cpp
@@ -14,6 +14,12 @@
 //     }
 // }
 
+// //  *  *  *  *  *  *
+// //  *  *  *  *  *  *
+// //  *  *  *  *  *  *
+// //  *  *  *  *  *  *
+// //  *  *  *  *  *  *
+
 // #include <iostream>
 // using namespace std;
 // int main()
@@ -37,6 +43,12 @@
 //     }
 // }
 
+// //  *  *  *  *  *
+// //  *           *
+// //  *           *
+// //  *           *
+// //  *  *  *  *  *
+
 // #include <iostream>
 // using namespace std;
 // int main()
@@ -52,6 +64,12 @@
 //         cout << endl;
 //     }
 // }
+
+// //  *  *  *  *  *
+// //  *  *  *  *
+// //  *  *  *
+// //  *  *
+// //  *
 
 // #include <iostream>
 // using namespace std;
@@ -76,6 +94,12 @@
 //     }
 // }
 
+// //              *
+// //           *  *
+// //        *  *  *
+// //     *  *  *  *
+// //  *  *  *  *  *
+
 // #include <iostream>
 // using namespace std;
 // int main()
@@ -93,6 +117,12 @@
 //         cout << endl;
 //     }
 // }
+
+// // 1
+// // 2  3
+// // 4  5  6
+// // 7  8  9  10
+// // 11  12  13  14  15
 
 // #include <iostream>
 // using namespace std;
@@ -136,11 +166,22 @@
 //     }
 // }
 
+// // *        *
+// // **      **
+// // ***    ***
+// // ****  ****
+// // **********
+// // **********
+// // ****  ****
+// // ***    ***
+// // **      **
+// // *        *
+
 // #include <iostream>
 // using namespace std;
 // int main()
 // {
-//     int row;
+//     int n;
 //     cin >> n;
 //     for (int i = 1; i <= n; i++)
 //     {
@@ -152,9 +193,14 @@
 //     }
 // }
 
+// // 1 2 3 4 5
+// // 1 2 3 4
+// // 1 2 3
+// // 1 2
+// // 1
+
 // #include <iostream>
 // using namespace std;
-
 // int main()
 // {
 //     int n;
@@ -176,6 +222,12 @@
 //     }
 // }
 
+// // 1
+// // 01
+// // 101
+// // 0101
+// // 10101
+
 // #include <iostream>
 // using namespace std;
 // int main()
@@ -195,3 +247,136 @@
 //         cout << endl;
 //     }
 // }
+
+// //     *****
+// //    *****
+// //   *****
+// //  *****
+// // *****
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= n - i; j++)
+//         {
+//             cout << " ";
+//         }
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << j << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// //     1
+// //    1 2
+// //   1 2 3
+// //  1 2 3 4
+// // 1 2 3 4 5
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n, k;
+//     cin >> n;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= n - i; j++)
+//         {
+//             cout << "  ";
+//         }
+//         for (int j = i; j >= 1; j--)
+//         {
+//             cout << j << " ";
+//         }
+//         for (int j = 2; j <= i; j++)
+//         {
+//             cout << j << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// //         1
+// //       2 1 2
+// //     3 2 1 2 3
+// //   4 3 2 1 2 3 4
+// // 5 4 3 2 1 2 3 4 5
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= n - i; j++)
+//         {
+//             cout << " ";
+//         }
+//         for (int j = 1; j <= 2 * i - 1; j++)
+//         {
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+//     for (int i = n; i >= 1; i--)
+//     {
+//         for (int j = 1; j <= n - i; j++)
+//         {
+//             cout << " ";
+//         }
+//         for (int j = 1; j <= 2 * i - 1; j++)
+//         {
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// //     *
+// //    ***
+// //   *****
+// //  *******
+// // *********
+// // *********
+// //  *******
+// //   *****
+// //    ***
+// //     *
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 1; i <= 3; i++)
+//     {
+//         for (int j = 1; j <= n; j++)
+//         {
+//             if (((i + j) % 4 == 0) || (i == 2 && j % 4 == 0))
+//             {
+//                 cout << "* ";
+//             }
+//             else
+//             {
+//                 cout << "  ";
+//             }
+//         }
+//         cout << endl;
+//     }
+// }
+
+// // 9
+// //     *       *
+// //   *   *   *   *
+// // *       *       *
