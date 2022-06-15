ea996123e945cf3504b3b7774d561a135529196a
0xsagar <sagar.19bct@cmr.edu.in>
2022-06-16 00:41:30 +0530

    codetantra pattern *A*

--- patterns.cpp
+++ patterns.cpp
@@ -380,3 +380,35 @@
 // //     *       *
 // //   *   *   *   *
 // // *       *       *
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = n - i; j > 0; j--)
+//         {
+//             cout << " ";
+//         }
+//         for (int j = 0; j <= i; j++)
+//         {
+//             if (i != 0 && j != i)
+//             {
+//                 cout << "*A";
+//             }
+//             else
+//             {
+//                 cout << "* ";
+//             }
+//         }
+//         cout << endl;
+//     }
+// }
+
+// //     *
+// //    *A*
+// //   *A*A*
+// //  *A*A*A*
