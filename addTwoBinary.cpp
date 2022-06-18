47a9a9c0cf3d03c18c93ca3e18680cc0351a9c96
0xsagar <sagar.19bct@cmr.edu.in>
2022-06-18 23:58:37 +0530

    adding binary

--- addTwoBinary.cpp
+++ addTwoBinary.cpp
@@ -93,7 +93,27 @@ int addTwoBinary(int a, int b)
         ans = ans * 10 + 1;
     }
     ans = reverse(ans);
-    return ans;
+    return ans;while (a > 0)
+    {
+        if (prevCarry == 1)
+        {
+            if (a % 2 == 1)
+            {
+                ans = ans * 10 + 0;
+                prevCarry = 1;
+            }
+            else
+            {
+                ans = ans * 10 + 1;
+                prevCarry = 0;
+            }
+        }
+        else
+        {
+            ans = ans * 10 + (a % 2);
+        }
+        a /= 10;
+    }
 }
 
 int main()
