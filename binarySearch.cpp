2d1ea173ddd636453f28c9ba6d584f2d93f3c451
0xsagar <sagar.19bct@cmr.edu.in>
2022-06-22 00:00:29 +0530

    day 11: binary search

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -10,22 +10,34 @@ int binarySearch(int arr[], int n, int key)
         int mid = (s + e) / 2;
         if (arr[mid] == key)
         {
+            cout << "posiion in array : ";
             return mid;
         }
         else if (arr[mid] > key)
         {
             e = mid - 1;
         }
+        else
+        {
+            s = mid + 1;
+        }
     }
+    return -1;
 }
 
 int main()
 {
     int n;
+    cout << "n : ";
     cin >> n;
     int arr[n];
     for (int i = 0; i < n; i++)
     {
+        cout << "element " << i << " : ";
         cin >> arr[i];
     }
+    int key;
+    cout << "key : ";
+    cin >> key;
+    cout << binarySearch(arr, n, key) << endl;
 }
