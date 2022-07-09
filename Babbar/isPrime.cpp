99c2ff043b4b3fccdaa2713bc97b43fec7dc6b32
0xsagar <sagar.19bct@cmr.edu.in>
2022-07-09 23:45:45 +0530

    is it prime

---
+++ Babbar/isPrime.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool prime(int n)
+{
+    for (int i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (prime(n) == 0)
+    {
+        cout << "Not Prime";
+    }
+    else
+    {
+        cout << "Prime";
+    }
+}
