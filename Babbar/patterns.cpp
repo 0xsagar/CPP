6fab40fcb3c8d97cd61c7b617c3d083108881457
0xsagar <sagar.19bct@cmr.edu.in>
2022-07-11 22:58:58 +0530

    patterns

---
+++ Babbar/patterns.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+using namespace std;
+// int main()
+// {
+//     int a;
+//     cin >> a;
+//     for (int i = 1; i <= a; i++)
+//     {
+//         for (int j = 1; j <= a; j++)
+//         {
+//             cout << j;
+//         }
+//         cout << endl;
+//     }
+// }
+
+// 1234
+// 1234
+// 1234
+// 1234
+
+// int main()
+// {
+//     int a;
+//     cin >> a;
+//     for (int i = 1; i <= a; i++)
+//     {
+//         for (int j = a; j >= 1; j--)
+//         {
+//             cout << j;
+//         }
+//         cout << endl;
+//     }
+// }
+
+// 4321
+// 4321
+// 4321
+// 4321
+
+// int main()
+// {
+//     int a, count = 1;
+//     cin >> a;
+//     for (int i = 1; i <= a; i++)
+//     {
+//         for (int j = 1; j <= a; j++)
+//         {
+//             cout << count;
+//             count++;
+//         }
+//         cout << endl;
+//     }
+// }
+
+// 123
+// 456
+// 789
+
+// int main()
+// {
+//     int a;
+//     cin >> a;
+//     for (int i = 1; i <= a; i++)
+//     {
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// *
+// **
+// ***
+
+// int main()
+// {
+//     int a;
+//     cin >> a;
+//     for (int i = 1; i <= a; i++)
+//     {
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << i;
+//         }
+//         cout << endl;
+//     }
+// }
+
+// 1
+// 22
+// 333
+// 4444
+
+// int main()
+// {
+//     int a, count = 1;
+//     cin >> a;
+//     for (int i = 1; i <= a; i++)
+//     {
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << count;
+//             count++;
+//         }
+//         cout << endl;
+//     }
+// }
+
+// 1
+// 23
+// 456
+// 78910
+
+// int main()
+// {
+//     int a;
+//     cin >> a;
+//     for (int i = 1; i <= a; i++)
+//     {
+//         for (int j = 0; j < i; j++)
+//         {
+//             cout << i + j;
+//         }
+//         cout << endl;
+//     }
+// }
+
+// 1
+// 23
+// 345
+// 4567
+// 56789
