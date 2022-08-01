118f693a5094f8d046a6dc8dccf834d9465043c2
0xsagar <sagar.19bct@cmr.edu.in>
2022-08-01 14:06:52 +0530

    sum of three in array equal to given s

---
+++ Babbar/tripleSum.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, s;
+    cin >> n >> s;
+    int arr[100];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            for (int k = j + 1; k < n; k++)
+            {
+                if (arr[i] + arr[j] + arr[k] == s)
+                {
+                    cout << arr[i] << " " << arr[j] << " " << arr[k] << endl;
+                }
+            }
+        }
+    }
+    return 0;
+}
