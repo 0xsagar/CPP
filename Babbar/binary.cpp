e28bc2fe38e7091ea0b3bea0f170aa73d9e352fc
0xsagar <sagar.19bct@cmr.edu.in>
2022-10-09 17:22:24 +0530

    ..

---
+++ Babbar/binary.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int n, int k, int arr[])
+{
+    int first = arr[0];
+    int last = arr[n];
+    int mid =
+}
+
+int main()
+{
+    int n, k;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+    int arr[100];
+    cout << "Enter array elements: ";
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Key: ";
+    cin >> k;
+}
--- Babbar/output.cpp
+++ Babbar/output.cpp
@@ -1,18 +1,80 @@
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     string str;
+//     cin >> str;
+//     int i, j, count = 0;
+//     j = str.size() - 1;
+//     for (int i = 0; i < j; i++)
+//     {
+//         if (str[i] == str[j])
+//         {
+//             count = count + 2;
+//         }
+//         j--;
+//     }
+//     cout << count;
+// }
+
+// FLEXCAR coding question
+#include <bits/stdc++.h>
+#define ll long long int
 using namespace std;
+ll maxSubArraySum(ll a[], ll size)
+{
+    ll max_so_far = INT_MIN, max_ending_here = 0;
+
+    for (ll i = 0; i < size; i++)
+    {
+        max_ending_here = max_ending_here + a[i];
+        if (max_so_far < max_ending_here)
+        {
+            max_so_far = max_ending_here;
+        }
+
+        if (max_ending_here < 0)
+        {
+            max_ending_here = 0;
+        }
+    }
+    return max_so_far;
+}
+
 int main()
 {
-    string str;
-    cin >> str;
-    int i, j, count = 0;
-    j = str.size() - 1;
-    for (int i = 0; i < j; i++)
+
+    ll t;
+    cin >> t;
+    while (t--)
     {
-        if (str[i] == str[j])
+
+        int n;
+        cin >> n;
+        ll A[n];
+
+        for (ll i = 0; i < n; i++)
         {
-            count = count + 2;
+            cin >> A[i];
         }
-        j--;
+
+        ll count = 0;
+        for (ll i = 0; i < n; i++)
+        {
+            if (A[i] < 0)
+            {
+                count++;
+            }
+        }
+        if (count == n) // Handling case when all the shops arevthe one where coins are deposited
+        {
+            cout << 0 << "\n";
+        }
+        else // Shops of the contiguous of both types
+        {
+            ll maxsum = maxSubArraySum(A, n);
+            cout << maxsum << "\n";
+        }
     }
-    cout << count;
+    return 0;
 }
